e2c59de96cc2c75b2d4f694133df69477ef7ad86
James Pamplona <pamplonajames@gmail.com>
2023-08-30 16:23:40 -0700

    Limit platform versions to iOS 16 platform version family.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Flow",
+    platforms: [.iOS(.v16), .macOS(.v13), .macCatalyst(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
