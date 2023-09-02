1faff00a507a19423184e939819587f08c4867b4
James Pamplona <pamplonajames@gmail.com>
2023-09-02 13:36:28 -0700

    Add a super basic smoke test to make sure sizeThatFits and getRects return the same extents.

--- Tests/FlowTests/FlowTests.swift
+++ Tests/FlowTests/FlowTests.swift
@@ -1,11 +1,20 @@
 import XCTest
+import struct SwiftUI.ProposedViewSize
 @testable import Flow
 
+
 final class FlowTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Flow().text, "Hello, World!")
+    func testSizeThatFitsMatchesGetRectsBoundingBox() throws {
+        let flow = Flow(alignment: .topLeading, spacing: 7)
+        let subviewSizes = (0..<20).map { _ in CGSize(width: Int.random(in: 40...200), height: Int.random(in: 30...90)) }
+
+        let sizeThatFits = flow.sizeThatFits(proposal: ProposedViewSize(width: 410, height: 1000), subviewSizes: subviewSizes)
+
+        let placedSizes = flow.getRects(for: subviewSizes, in: CGRect(origin: .zero, size: sizeThatFits))
+
+        let placedSizesBoundingBox = placedSizes.reduce(placedSizes[0]) { $0.union($1)
+        }
+
+        XCTAssertEqual(sizeThatFits, placedSizesBoundingBox.size)
     }
 }
