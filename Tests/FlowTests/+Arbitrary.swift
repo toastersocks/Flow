b386162d1ac40548b3e8d98428dbbf59379b7cab
James Pamplona <pamplonajames@gmail.com>
2023-09-26 14:56:57 -0700

    Style tweak. Remove unnecessary module specifier.

--- Tests/FlowTests/+Arbitrary.swift
+++ Tests/FlowTests/+Arbitrary.swift
@@ -9,7 +9,7 @@ import CoreGraphics
 import SwiftCheck
 
 extension CGSize: Arbitrary {
-    public static var arbitrary: SwiftCheck.Gen<CGSize> {
+    public static var arbitrary: Gen<CGSize> {
         Gen<CGSize>.compose { composer in
             CGSize(width: composer.generate(using: Double.arbitrary.suchThat { $0 >= 0 && $0 <= 410 }),
                    height: composer.generate(using: Double.arbitrary.suchThat { $0 >= 0 && $0 <= 1000 }))
