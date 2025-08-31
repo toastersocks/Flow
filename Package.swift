55ebc9e7c197af781763fc2585272237a06cc55c
James Pamplona <pamplonajames@gmail.com>
2025-08-30 20:55:11 -0700

    Commented out code cleanup

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/toastersocks/SwiftCheck", .upToNextMajor(from: "0.13.0")),
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
